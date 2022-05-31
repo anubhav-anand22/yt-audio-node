fcd225417484ea88f29bed7b69fe91437611d6a6
ak7488 <anubhav7488599737185@gmail.com>
2022-05-31 10:28:38 +0530

    added stay awake route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const {audioRouter} = require('./Routers/audioRouter');
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT;
@@ -12,6 +13,23 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(audioRouter)
 
 app.get('/ok', (req, res) => res.send('ok'));
+
+app.get('/delay-res', (req, res) => {
+    try {
+        const you = req.query.you;
+        const me = req.query.me
+
+        if(!you || !me) return res.status(400).send('No url provided');
+
+        setTimeout(() => {
+            res.send({ok: 'ok'})
+            axios(`${you}?me=${you}&you=${me}`);
+        }, 10000);
+    } catch (e) {
+        res.status(500).send();
+        console.log(e);
+    }
+})
 
 app.listen(port, () => {
     console.log(`server is up and running on port ${port}`);
